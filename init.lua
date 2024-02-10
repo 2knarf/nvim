5b93427f804157fe47368f088ba2c531cb9e6bbf
Frank Solli <you@example.com>
2024-02-10 15:41:12 +0100

    telescope in, nerdtree out

--- init.lua
+++ init.lua
@@ -85,3 +85,8 @@ cmp.setup({
     ['<C-b>'] = cmp_action.luasnip_jump_backward(),
   })
 })
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
